c0a41668913a7dc7508ff2f307fa04d85bacabee
Efrain Perez <efrain.abperez23@gmail.com>
2018-05-13 01:45:47 -0500

    added header and translation service moved to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CoreRoutingModule } from './core-routing.module';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CoreRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    HttpClientModule,
+    NgbModule.forRoot(),
   ],
-  declarations: []
+  declarations: [
+    NavComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  exports: [HomeComponent, TranslateModule, NgbModule]
 })
 export class CoreModule { }
--- src/app/global/global.module.ts
+++ src/app/global/global.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { GlobalRoutingModule } from './global-routing.module';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    GlobalRoutingModule
   ],
   declarations: []
 })
